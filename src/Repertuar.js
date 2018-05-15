ca57b2aaebc6dbc8439224e9cca66bf36a54b640
Jakub Kwiatek <kwiatekjakub52@gmail.com>
2018-05-15 16:44:37 +0200

    firebase

--- src/Repertuar.js
+++ src/Repertuar.js
@@ -11,44 +11,63 @@ class Repertuar extends Component {
         super(props);
         this.state = {
             array: [],
-            firebaseArr: [],
-        }
+			titles: [],
+		 }
     }
 
     componentDidMount(){
         let tempArr = [];
         let arr = [];
-        const ref = app.database().ref().child("seances");
-        let promise = new Promise(() => {
-            ref.once('value').then( snap => {
-                snap.forEach(childSnap => arr.push(childSnap.val())); });
-        })
-        promise.then(() => {
-            this.setState({firebaseArr: arr})
-        });
+		
+		const context = this;
+		
         let options = { weekday: 'short', month: '2-digit', day: 'numeric' };
-        for (let i=0; i<7; i++) {
+        
+		for (let i=0; i<7; i++) {
             let date = new Date();
             date.setDate(date.getDate() + i)
             tempArr.push(date.toLocaleDateString("pl-PL",options))
         }
-        console.log(arr);
-        this.setState({ array: tempArr });
+      
+		this.setState({ array: tempArr });
+		
+        const ref = app.database().ref().child("seances");
+    
+		ref.on('value', function(snapshot){
+			arr = [];
+			snapshot.forEach(function(childSnapshot) {
+				arr.push(childSnapshot.val());
+			})
+			printMoviesTitles(arr);
+		})
+		
+		function printMoviesTitles(seances){
+			let moviesTitles = [];
+			seances.forEach(seance => {
+				if(!ifStringExists(seance.title, moviesTitles)){
+					moviesTitles.push(seance.title);
+				}
+			})	
+			context.setState({ titles: moviesTitles});
+		}
+		
+		function ifStringExists(string, array){
+			return (array.indexOf(string) > -1);
+		}
     }
 
     render() {
-        console.log(this.state.firebaseArr, this.state.array);
+	
         return (
             <React.Fragment>
                 <table>
                     <tbody>
                         <tr>
+							<th>Title</th>
                             { this.state.array.map(day => <th>{day}</th>) }
                         </tr>
-                        <tr>
-                            <td> something </td>
-                        </tr>
-                    </tbody>
+						    { this.state.titles.map(title => <tr><th>{title}</th></tr>) }    
+                   </tbody>
                 </table>
             </React.Fragment>
         )
